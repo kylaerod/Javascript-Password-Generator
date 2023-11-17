3c45bf67581ff6df4eb5d74d9af6e053ce22e274
kylaerod <kylarodriguez52@gmail.com>
2023-11-16 23:26:57 -0800

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Password Generator Starter Code
+# Javascript-Password-Generator
+
+## Description
+
+This project was made to create a password generator for the client who was looking to create secure and randomly generated passowrds.
+
+## Installation
+
+This project was completed using starter code from gitHub by username friendly-parakeet. It was then improved with html semantics. The only adjustment to code was either the removal of repetative functions, the renaming of divs, or the adjustment of spacing.
+
+## Usage
+
+After cloning the starter code to my computer and into my repository, more JavaScript was created in order to communicate the needs of the client. A new password needed to be created that was between 8 and 128 characters long, inlude lowercase, uppercase, numbers, and special characters, all given options to the user. 
+
+We then see randomly generated passwords provided onto the page.
+
+![Window Open](<JSSC2.png>)
+![Result](<!JSSC1.png>)
+
+## Credits
+
+The starter code was provided by gitHub unsername friendly-parakeet, whos gitHub is linked below along with the starter code I used for this project.
+
+https://github.com/coding-boot-camp/friendly-parakeet
+
+I was also provided this resource as well as instruction from the University of Minnesota's Coding bootcamp administration.
+
+https://bootcampspot.instructure.com/courses/4983/assignments/64676?module_item_id=1115732
+
+W3schools.com also provided my insight on the best parameters to allow my code to function properly on the page, especially with syntax.
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Kyla
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,84 +1,58 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Characters that can be used to create the password
-var charactertypes = {
-  lowerCase:'abcdefghijklmnopqrstuvwxyz'
-  upperCase:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  numeric: '0123456789'
-  speacialCharacters:'~!@#$%^&*()_+<>/'
-}
+// Write password to the input
+function writePassword() {
 
-// Password Parameters
-var passData = {
-  length: 0
-  lowerCase: true
-  upperCase: true
-  numeric: true
-  speacialCharacters: true
-}
+  // Define character sets
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numericChars = '0123456789';
+  const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
 
-function generatePassword() {
-resetPassData();
-passLength ();
-passCharacterTypes ();
+  // Prompt for password length
+  let passwordLength = parseInt(prompt('Enter password length (between 8 and 128):'));
 
-
-if (!passData.lowerCase &&
-  !passData.upperCase &&
-  !passData.numeric &&
-  !passData.speacialCharacters &&)
-
-  { alert ("Would you like to get a password or make a new one?");
-    generatePassword();
-} else {
-  makePassWord ();
-}
-return pass; }
-
-function resetPassData () {
-  passArray = [];
-  pass = ''
-  passData.length = 0 ;
-}
-
-var pass = ''
-
-function makePassWord() {
-for (var i = 0; i < passData.length; i++)
-{ pass += passArray [Math.floor(math.random() * passArray.length)];
-}
-return;  
-}
-
-function passLength() {
-  var length = prompt ("Choose a length for your password.")
-  if (length <8 || length > 128)
-  { alert (Your password must be between 8 and 128 characters"); 
-} else {
-  passData.length = length;
-}
-}
-function passCharacterTypes() {
-  for (var type in characterTypes) {
-    if (confirm ("Do you want your password to contain " + type + " ?")) {
-      passData[type] = true;
-      passArray = passArray.concat(charactertypes[type].split(""))
-    } else {}
+  // Validate password length
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt('Invalid length. Please enter a number between 8 and 128:'));
   }
+
+  // Confirm character types
+  const includeLowercase = confirm('Include lowercase characters?');
+  const includeUppercase = confirm('Include uppercase characters?');
+  const includeNumeric = confirm('Include numeric characters?');
+  const includeSpecial = confirm('Include special characters?');
+
+  // Validate at least one character type is selected
+  while (!(includeLowercase || includeUppercase || includeNumeric || includeSpecial)) {
+    alert('Please select at least one character type.');
+    // Confirm character types again
+    includeLowercase = confirm('Include lowercase characters?');
+    includeUppercase = confirm('Include uppercase characters?');
+    includeNumeric = confirm('Include numeric characters?');
+    includeSpecial = confirm('Include special characters?');
+  }
+
+  // Link selected character sets
+  let characters = '';
+  if (includeLowercase) characters += lowercaseChars;
+  if (includeUppercase) characters += uppercaseChars;
+  if (includeNumeric) characters += numericChars;
+  if (includeSpecial) characters += specialChars;
+
+  // Generate password
+  let password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+
+  // Display the generated password on the screen
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
